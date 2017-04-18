72e519c93d88d915d44168661a76494ca27ca6e5
leoloveacm <1064944119@qq.com>
2017-04-18 18:02:01 +0800

    finish

---
+++ index.js
@@ -0,0 +1,36 @@
+var http = require("http")
+var fs = require("fs")
+var cheerio = require("cheerio")
+var iconv = require("iconv-lite")
+var url = 'http://m.bp17.com/164/'
+http.get(url, function(res) {  //资源请求
+    var chunks = []
+    res.on('data', function(chunk) {
+        chunks.push(chunk)
+    })
+    res.on('end', function() {
+        var html = iconv.decode(Buffer.concat(chunks), 'gb2312') //转码操作
+        var $ = cheerio.load(html, {
+            decodeEntities: false
+        })
+        var content = $("tbody")
+        var links = []
+        $('div').children('ul').children('li').children('a').each(function(i, elem) {
+            var link = new Object()
+            link.title = $(this).text()
+            link.link = 'http://m.bp17.com' + $(this).attr('href') //补齐 URL 信息
+            if (i > 4 && i < 14) {
+                links.push(link)
+            }
+        })
+        fs.writeFile("list.json", JSON.stringify(links), function(err) {
+            if (!err) {
+                console.log("写文件成功")
+            }
+        })
+    }).on('error', function() {
+        console.log("网页访问出错")
+    })
+})
+
+
---
+++ main.js
@@ -0,0 +1,50 @@
+var http = require("http")
+var fs = require("fs")
+var cheerio = require("cheerio")
+var iconv = require("iconv-lite")
+var path = require('path')
+var urlList = JSON.parse(fs.readFileSync('list.json', 'utf8'))
+var filepath = '门房秦大爷的故事'
+function getContent(chapter,cnt) {
+    console.log(chapter.link)
+    http.get(chapter.link, function(res) {
+        var chunks = []
+        res.on('data', function(chunk) {
+            chunks.push(chunk)
+        })
+        res.on('end', function() {
+            var html = iconv.decode(Buffer.concat(chunks), 'gb2312')
+            var $ = cheerio.load(html, {
+                decodeEntities: false
+            })
+            var content = ($(".txt").text()).replace(/\&nbsp;/g, '')
+            // console.log("%d\n",cnt)
+            var filename = '门房秦大爷的故事' + cnt.toString() + '.md'
+
+            if (fs.existsSync(filename)) {
+                fs.appendFileSync(filename, '### ' + chapter.title)
+                fs.appendFileSync(filename, content)
+            } else {
+                fs.writeFileSync(filename, '### ' + chapter.title)
+                fs.appendFileSync(filename, content)
+            }
+            fs.rename(filename,filepath + '\\' + filename,function(err){
+                if(err){
+                    console.log('something wrong' + err)
+                }
+                console.log('done')
+            })
+
+        })
+    }).on('error', function() {
+        console.log("爬取" + chapter.link + "链接出错！")
+    })
+}
+if(!fs.exists(filepath)){
+    fs.mkdir(filepath)
+}
+// console.log(urlList.length)
+for (let i = 0; i < urlList.length; i++) {
+    console.log(urlList[i])
+    getContent(urlList[i],i)
+}
